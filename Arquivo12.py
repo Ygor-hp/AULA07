7fd7f0ed1466e9964a723f2f2c0c8334a00f112e
ygor emmanoel <ygoremmanoel23@gmail.com>
2025-05-06 11:36:15 -0300

    finalizado

--- Arquivo12.py
+++ Arquivo12.py
@@ -1,3 +1,5 @@
+from AULA07.Arquivo05 import media
+
 n=[0]*10
 for i in range(10):
     n[i]=int(input("digite um numero:"))
@@ -14,3 +16,13 @@ for x in range(1,10):
         maior=n[x]
 print(f"\nmenor valor:{menor}")
 print(f"\nmaior valor: {maior}")
+soma=0
+for k in range(10):
+    soma+=n[k]
+media=soma/10
+maiorquemrdia=0
+for p in range(10):
+    if n[p]>media:
+        maiorquemrdia+=1
+print(f" media dos valores:{media}")
+print(f"quantidade de valores maior que media es media:{maiorquemrdia}")
